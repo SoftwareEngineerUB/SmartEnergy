d457e2ab71049eac08e403add0b8cb720f3b71ea
JohnnySavu <johnny.savu.99@gmail.com>
2022-01-28 13:42:13 +0200

    fix for deviceLeftRunning

--- app/meter/device.py
+++ app/meter/device.py
@@ -136,15 +136,15 @@ class DeviceObject:
     # predicts if this device is using much more energy than usual, and asks the user if he forgot the device in the running state
     # prediction is valid for the last 1.5 hours of running
     def isDeviceLeftRunning(self):
-        queryString = f"SELECT * FROM data WHERE `device_id` = {self.device_id} and strftime('%Y',`time`) = '2015' ORDER BY `time` DESC LIMIT 10"
+        queryString = f"SELECT * FROM data WHERE `device_id` = {self.device_id} and strftime('%Y',`time`) = '2015' ORDER BY `time` DESC LIMIT 3"
         query = text(queryString)
         cursor = db.engine.execute(query)
 
         data = [dict(row.items()) for row in cursor]
         data = [[datapoint['time'], datapoint['value']] for datapoint in data]
+
         start_time = data[0][0]
         end_time = data[-1][0]
-
 
         anomalyDetector = AnomalyDetector(self.device_id)
         predictedConsumption = anomalyDetector.predictConsumption(start_time, end_time)
